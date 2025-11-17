e398665937d5dd9dca8d2e60c23f39d10e34de3c
Sungdo <nosd0644@naver.com>
2025-11-17 23:23:16 +0900

    d

--- src/QuizData.js
+++ src/QuizData.js
@@ -2,4 +2,5 @@ export const QuizData = [
   { id: 1, a: 'sundo', imgUrl:'https://imgur.com/a/jqzkAE3' }
   //{ id: 2, q: 'who are you', a: 'me' },
   //{ id: 3, q: 'who are you2', a: 'me' }
+  //{ id: 3, q: 'who are you2', a: 'me' }
 ];
